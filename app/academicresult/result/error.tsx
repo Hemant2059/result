7d78b8b19a292f4932d3663c6b9671ec65d4969f
Hemant <hemantpandey1029@gmail.com>
2024-06-24 10:51:55 +0530

    error page fix

--- app/academicresult/result/error.tsx
+++ app/academicresult/result/error.tsx
@@ -5,8 +5,8 @@ import React from "react";
 
 const error = () => {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <h1>Something Went Wrong</h1>
+    <div className="h-screen flex flex-col items-center justify-center space-y-4">
+      <h1>HallTicket Not Found</h1>
       <Link href={"/"}>
         <Button>Go Back</Button>
       </Link>
