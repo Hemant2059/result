420e598395aeedf924fd168485ca7d748a805e31
Hemant2059 <98577177+Hemant2059@users.noreply.github.com>
2024-07-05 22:34:32 +0545

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <SpeedInsights />
-      <Analytics/>
+      
       <body className={inter.className}>{children}</body>
     </html>
   );
